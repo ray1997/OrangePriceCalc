1e516d394d6c284994e28762c88bdf8d52f86e8c
Toon Werawat <14814401+ray1997@users.noreply.github.com>
2024-08-18 10:33:38 +0700

    Update Program.cs
    Force app to Thai

--- Program.cs
+++ Program.cs
@@ -8,6 +8,13 @@ builder.Services.AddFluentUIComponents();
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
+// Set the culture globally to Thai
+var culture = new CultureInfo("th-TH");
+CultureInfo.DefaultThreadCurrentCulture = culture;
+CultureInfo.DefaultThreadCurrentUICulture = culture;
+
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
