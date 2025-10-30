eea8452e24577de60ffd3b13ccb438657743e87a
Toon Werawat <14814401+ray1997@users.noreply.github.com>
2025-10-31 01:31:36 +0700

    Forgot to remove context in api

--- OrangeWebAPI/Helper/JsonContext.cs
+++ OrangeWebAPI/Helper/JsonContext.cs
@@ -5,7 +5,6 @@ namespace OrangeWebAPI.Helper;
 [JsonSerializable(typeof(OrangeAPICore.DatabaseInfo))]
 [JsonSerializable((typeof(string)))]
 [JsonSerializable((typeof(decimal)))]
-[JsonSerializable((typeof(OrangeAPICore.DiscountSteps)))]
 [JsonSourceGenerationOptions(WriteIndented = true)]
 
 public partial class AppJsonContext : JsonSerializerContext
