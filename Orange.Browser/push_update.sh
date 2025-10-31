ab8d97869f653018bfce1f44c3cc0cd9e7963baf
Toon Werawat <14814401+ray1997@users.noreply.github.com>
2025-11-01 01:40:08 +0700

    Remove logo + update script

--- Orange.Browser/push_update.sh
+++ Orange.Browser/push_update.sh
@@ -1,1 +1,5 @@
+dotnet publish -c Release 
+ssh dietpi@192.168.1.160 "rm -rf ~/discountwww/*"
 rsync -avz --progress ~/RiderProjects/OrangePriceCalc/Orange.Browser/bin/Release/net9.0-browser/publish/wwwroot dietpi@192.168.1.160:~/discountwww/
+ssh dietpi@192.168.1.160 "cd ~/discountwww && mv wwwroot/* ./ && rm -rf wwwroot"
+sudo shutdown now
